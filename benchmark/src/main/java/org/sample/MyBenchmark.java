2ccd8b5fc83d8b4f5523946afdc192f6eccae897
Simon Massey <simon@simonmassey.org>
2025-05-01 23:17:44 +0100

    benchmarks

---
+++ benchmark/src/main/java/org/sample/MyBenchmark.java
@@ -0,0 +1,41 @@
+package org.sample;
+
+import io.github.simbo1905.simple_pickle.Pickler;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Thread)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS) // 1 warmup iteration, 1 second long
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS) // 1 measurement iteration, 1 second long
+@Fork(1) // Run in 1 fork
+//@BenchmarkMode(Mode.AverageTime) // Optional: Specify mode if needed
+//@OutputTimeUnit(TimeUnit.NANOSECONDS) // Optional: Specify time unit for results
+public class MyBenchmark {
+
+  final ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+  // This method runs BEFORE EACH call to helloWorld()
+  @Setup(Level.Invocation)
+  public void setupInvocation() {
+    buffer.clear();
+  }
+
+  @Benchmark
+  public void helloWorld(Blackhole bh) {
+    final List<Push> original = List.of(
+        new Push("hello"),
+        new Push("world"),
+        new Push("yes"),
+        new Push("no"));
+    int size = Pickler.sizeOfList(Push.class, original);
+    ByteBuffer buffer = ByteBuffer.allocate(size);
+    Pickler.serializeList(Push.class, original, buffer);
+    buffer.flip();
+    final var back = Pickler.deserializeList(Push.class, buffer);
+    bh.consume(back);
+  }
+}
---
+++ benchmark/src/main/java/org/sample/Push.java
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: 2025 Simon Massey
+// SPDX-License-Identifier: Apache-2.0
+package org.sample;
+
+public record Push(String item) implements StackCommand {
+}
