ff9c6c2107bc9909b82df181019ed2bdbacb89de
Simon Massey <simon@simonmassey.org>
2025-05-05 07:32:32 +0100

    tidy-up

--- benchmark/src/main/java/org/sample/InternalNode.java
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2025 Simon Massey
-// SPDX-License-Identifier: Apache-2.0
-package org.sample;
-
-import java.io.Serializable;
-import java.util.Objects;
-
-/// Internal node that may have left and right children
-
