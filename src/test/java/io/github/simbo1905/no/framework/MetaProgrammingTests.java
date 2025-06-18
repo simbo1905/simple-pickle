e5d389e0419936d1fb2c8fa28b2bd1f4dd167af9
Simon Massey <simon@simonmassey.org>
2025-06-18 17:36:14 +0100

    works

--- src/test/java/io/github/simbo1905/no/framework/MetaProgrammingTests.java
+++ src/test/java/io/github/simbo1905/no/framework/MetaProgrammingTests.java
@@ -39,118 +39,83 @@ public class MetaProgrammingTests {
     LOGGER.fine(() -> "Finished MetaProgrammingTests test");
   }
 
-  @Nested
-  @DisplayName("Writer Chain Tests")
-  class PrimitiveRoundTripTest {
-
-    @Test
-    @DisplayName("Test primitive chains")
-    void testPrimitives() {
-      // Get the method handle for anIntNotZero()
-      final @NotNull RecordComponent[] components = PrimitiveValueRecord.class.getRecordComponents();
-      // make an array of the component accessor methods
-      final @NotNull MethodHandle[] accessors = new MethodHandle[components.length];
-      final @NotNull TypeExpr[] typeExprs = new TypeExpr[components.length];
-
-      IntStream.range(0, components.length).forEach(i -> {
-        RecordComponent component = components[i];
-
-        // Get accessor method handle
-        try {
-          MethodHandle accessor = MethodHandles.lookup().unreflect(component.getAccessor());
-
-          // Log the method handle details
-          LOGGER.fine(() -> String.format("Accessor method handle %d: %s for component %s",
-              i, accessor.type(), component.getName()));
-
-          accessors[i] = accessor;
-        } catch (IllegalAccessException e) {
-          throw new RuntimeException("Failed to un reflect accessor for " + component.getName(), e);
-        }
-        // Analyze the type of the component
-        Type type = component.getGenericType();
-        typeExprs[i] = TypeExpr.analyze(type);
-      });
-
-      // Test boolean component
-      final TypeExpr typeExpr0 = typeExprs[0];
-      final MethodHandle typeExpr0Accessor = accessors[0];
-      testBooleanRoundTrip(typeExpr0, typeExpr0Accessor);
-
-      // Test byte component
-      final TypeExpr typeExpr1 = typeExprs[1];
-      final MethodHandle typeExpr1Accessor = accessors[1];
-      testByteRoundTrip(typeExpr1, typeExpr1Accessor);
-
-      // Test char component
-      final TypeExpr typeExpr2 = typeExprs[2];
-      final MethodHandle typeExpr2Accessor = accessors[2];
-      testCharRoundTrip(typeExpr2, typeExpr2Accessor);
-
-      // Test short component
-      final TypeExpr typeExpr3 = typeExprs[3];
-      final MethodHandle typeExpr3Accessor = accessors[3];
-      testShortRoundTrip(typeExpr3, typeExpr3Accessor);
-
-      // Test int component
-      final TypeExpr typeExpr4 = typeExprs[4];
-      final MethodHandle typeExpr4Accessor = accessors[4];
-      testIntRoundTrip(typeExpr4, typeExpr4Accessor);
-
-      // Test long component
-      final TypeExpr typeExpr5 = typeExprs[5];
-      final MethodHandle typeExpr5Accessor = accessors[5];
-      testLongRoundTrip(typeExpr5, typeExpr5Accessor);
-
-      // Test float component
-      final TypeExpr typeExpr6 = typeExprs[6];
-      final MethodHandle typeExpr6Accessor = accessors[6];
-      testFloatRoundTrip(typeExpr6, typeExpr6Accessor);
-
-      // Test double component
-      final TypeExpr typeExpr7 = typeExprs[7];
-      final MethodHandle typeExpr7Accessor = accessors[7];
-      testDoubleRoundTrip(typeExpr7, typeExpr7Accessor);
-    }
-
-    void testBooleanRoundTrip(TypeExpr typeExpr0, MethodHandle typeExpr0Accessor) {
-      LOGGER.fine(() -> "Type of first component: " + typeExpr0);
-      // We expect the first component to be a primitive type of boolean
-      assertThat(typeExpr0.isPrimitive()).isTrue();
-      // switch on it being boolean
-      if (typeExpr0 instanceof TypeExpr.PrimitiveValueNode e) {
-        LOGGER.fine(() -> "First component is boolean");
-        assertThat(e.type()).isEqualTo(TypeExpr.PrimitiveValueType.BOOLEAN);
-        // boolean.class
-        final var writerChain = PicklerUsingAst.buildPrimitiveValueWriter(e.type(), typeExpr0Accessor);
-        assertThat(writerChain).isNotNull();
-        // We can write the record to a ByteBuffer
-        final var byteBuffer = ByteBuffer.allocate(1024);
-        LOGGER.fine(() -> "Attempting to write boolean value to buffer");
-        try {
-          writerChain.accept(byteBuffer, primitiveValueRecord);
-        } catch (Throwable e2) {
-          LOGGER.severe(() -> "Failed to write boolean value: " + e2.getMessage());
-          throw new RuntimeException(e2);
-        }
-        byteBuffer.flip();
-        LOGGER.fine(() -> "Successfully wrote boolean value to buffer");
-        // Now we can read it back
-        final var readerChain = PicklerUsingAst.buildPrimitiveValueReader(e.type());
-        final var readValue = readerChain.apply(byteBuffer);
-        LOGGER.fine(() -> "Read boolean value: " + readValue);
-        // Check the value is as expected
-        assertThat(readValue).isEqualTo(primitiveValueRecord.booleanValue());
-        // check how much was written
-        final int bytesWritten = byteBuffer.position();
-        // check that the sizer will return the something greater than or equal to the bytes written
-        final var sizer = PicklerUsingAst.buildPrimitiveValueSizer(e.type(), typeExpr0Accessor);
-        final int size = sizer.applyAsInt(primitiveValueRecord);
-        LOGGER.fine(() -> "Bytes written: " + bytesWritten + ", Sizer returned: " + size);
-        assertThat(size).isGreaterThanOrEqualTo(bytesWritten);
-      } else {
-        throw new IllegalStateException("Unexpected value: " + typeExpr0);
+  void testBooleanRoundTrip(TypeExpr typeExpr0, MethodHandle typeExpr0Accessor) {
+    LOGGER.fine(() -> "Type of first component: " + typeExpr0);
+    // We expect the first component to be a primitive type of boolean
+    assertThat(typeExpr0.isPrimitive()).isTrue();
+    // switch on it being boolean
+    if (typeExpr0 instanceof TypeExpr.PrimitiveValueNode e) {
+      LOGGER.fine(() -> "First component is boolean");
+      assertThat(e.type()).isEqualTo(TypeExpr.PrimitiveValueType.BOOLEAN);
+      // boolean.class
+      final var writerChain = PicklerUsingAst.buildPrimitiveValueWriter(e.type(), typeExpr0Accessor);
+      assertThat(writerChain).isNotNull();
+      // We can write the record to a ByteBuffer
+      final var byteBuffer = ByteBuffer.allocate(1024);
+      LOGGER.fine(() -> "Attempting to write boolean value to buffer");
+      try {
+        writerChain.accept(byteBuffer, primitiveValueRecord);
+      } catch (Throwable e2) {
+        LOGGER.severe(() -> "Failed to write boolean value: " + e2.getMessage());
+        throw new RuntimeException(e2);
       }
+      byteBuffer.flip();
+      LOGGER.fine(() -> "Successfully wrote boolean value to buffer");
+      // Now we can read it back
+      final var readerChain = PicklerUsingAst.buildPrimitiveValueReader(e.type());
+      final var readValue = readerChain.apply(byteBuffer);
+      LOGGER.fine(() -> "Read boolean value: " + readValue);
+      // Check the value is as expected
+      assertThat(readValue).isEqualTo(primitiveValueRecord.booleanValue());
+      // check how much was written
+      final int bytesWritten = byteBuffer.position();
+      // check that the sizer will return the something greater than or equal to the bytes written
+      final var sizer = PicklerUsingAst.buildPrimitiveValueSizer(e.type(), typeExpr0Accessor);
+      final int size = sizer.applyAsInt(primitiveValueRecord);
+      LOGGER.fine(() -> "Bytes written: " + bytesWritten + ", Sizer returned: " + size);
+      assertThat(size).isGreaterThanOrEqualTo(bytesWritten);
+    } else {
+      throw new IllegalStateException("Unexpected value: " + typeExpr0);
+    }
+  }
+
+  void testByteRoundTrip(TypeExpr typeExpr, MethodHandle accessor) {
+    LOGGER.fine(() -> "Type of byte component: " + typeExpr);
+    // We expect the component to be a primitive type of byte
+    assertThat(typeExpr.isPrimitive()).isTrue();
+    // switch on it being byte
+    if (typeExpr instanceof TypeExpr.PrimitiveValueNode e) {
+      LOGGER.fine(() -> "Byte component is byte");
+      assertThat(e.type()).isEqualTo(TypeExpr.PrimitiveValueType.BYTE);
+      // byte.class
+      final var writerChain = PicklerUsingAst.buildPrimitiveValueWriter(e.type(), accessor);
+      assertThat(writerChain).isNotNull();
+      // We can write the record to a ByteBuffer
+      final var byteBuffer = ByteBuffer.allocate(1024);
+      LOGGER.fine(() -> "Attempting to write byte value to buffer");
+      try {
+        writerChain.accept(byteBuffer, primitiveValueRecord);
+      } catch (Throwable e2) {
+        LOGGER.severe(() -> "Failed to write byte value: " + e2.getMessage());
+        throw new RuntimeException(e2);
+      }
+      byteBuffer.flip();
+      LOGGER.fine(() -> "Successfully wrote byte value to buffer");
+      // Now we can read it back
+      final var readerChain = PicklerUsingAst.buildPrimitiveValueReader(e.type());
+      final var readValue = readerChain.apply(byteBuffer);
+      LOGGER.fine(() -> "Read byte value: " + readValue);
+      // Check the value is as expected
+      assertThat(readValue).isEqualTo(primitiveValueRecord.byteValue());
+      // check how much was written
+      final int bytesWritten = byteBuffer.position();
+      // check that the sizer will return the something greater than or equal to the bytes written
+      final var sizer = PicklerUsingAst.buildPrimitiveValueSizer(e.type(), accessor);
+      final int size = sizer.applyAsInt(primitiveValueRecord);
+      LOGGER.fine(() -> "Bytes written: " + bytesWritten + ", Sizer returned: " + size);
+      assertThat(size).isGreaterThanOrEqualTo(bytesWritten);
+    } else {
+      throw new IllegalStateException("Unexpected value: " + typeExpr);
     }
   }
 
@@ -161,7 +126,7 @@ public class MetaProgrammingTests {
     // switch on it being char
     if (typeExpr instanceof TypeExpr.PrimitiveValueNode e) {
       LOGGER.fine(() -> "Char component is char");
-      assertThat(e.type()).isEqualTo(TypeExpr.PrimitiveValueType.CHAR);
+      assertThat(e.type()).isEqualTo(TypeExpr.PrimitiveValueType.CHARACTER);
       // char.class
       final var writerChain = PicklerUsingAst.buildPrimitiveValueWriter(e.type(), accessor);
       assertThat(writerChain).isNotNull();
@@ -241,7 +206,7 @@ public class MetaProgrammingTests {
     // switch on it being int
     if (typeExpr instanceof TypeExpr.PrimitiveValueNode e) {
       LOGGER.fine(() -> "Int component is int");
-      assertThat(e.type()).isEqualTo(TypeExpr.PrimitiveValueType.INT);
+      assertThat(e.type()).isEqualTo(TypeExpr.PrimitiveValueType.INTEGER);
       // int.class
       final var writerChain = PicklerUsingAst.buildPrimitiveValueWriter(e.type(), accessor);
       assertThat(writerChain).isNotNull();
@@ -391,6 +356,81 @@ public class MetaProgrammingTests {
       assertThat(size).isGreaterThanOrEqualTo(bytesWritten);
     } else {
       throw new IllegalStateException("Unexpected value: " + typeExpr);
+    }
+  }
+
+  @Nested
+  @DisplayName("Writer Chain Tests")
+  class PrimitiveRoundTripTest {
+
+    @Test
+    @DisplayName("Test primitive chains")
+    void testPrimitives() {
+      // Get the method handle for anIntNotZero()
+      final @NotNull RecordComponent[] components = PrimitiveValueRecord.class.getRecordComponents();
+      // make an array of the component accessor methods
+      final @NotNull MethodHandle[] accessors = new MethodHandle[components.length];
+      final @NotNull TypeExpr[] typeExprs = new TypeExpr[components.length];
+
+      IntStream.range(0, components.length).forEach(i -> {
+        RecordComponent component = components[i];
+
+        // Get accessor method handle
+        try {
+          MethodHandle accessor = MethodHandles.lookup().unreflect(component.getAccessor());
+
+          // Log the method handle details
+          LOGGER.fine(() -> String.format("Accessor method handle %d: %s for component %s",
+              i, accessor.type(), component.getName()));
+
+          accessors[i] = accessor;
+        } catch (IllegalAccessException e) {
+          throw new RuntimeException("Failed to un reflect accessor for " + component.getName(), e);
+        }
+        // Analyze the type of the component
+        Type type = component.getGenericType();
+        typeExprs[i] = TypeExpr.analyze(type);
+      });
+
+      // Test boolean component
+      final TypeExpr typeExpr0 = typeExprs[0];
+      final MethodHandle typeExpr0Accessor = accessors[0];
+      testBooleanRoundTrip(typeExpr0, typeExpr0Accessor);
+
+      // Test byte component
+      final TypeExpr typeExpr1 = typeExprs[1];
+      final MethodHandle typeExpr1Accessor = accessors[1];
+      testByteRoundTrip(typeExpr1, typeExpr1Accessor);
+
+      // Test char component
+      final TypeExpr typeExpr2 = typeExprs[2];
+      final MethodHandle typeExpr2Accessor = accessors[2];
+      testCharRoundTrip(typeExpr2, typeExpr2Accessor);
+
+      // Test short component
+      final TypeExpr typeExpr3 = typeExprs[3];
+      final MethodHandle typeExpr3Accessor = accessors[3];
+      testShortRoundTrip(typeExpr3, typeExpr3Accessor);
+
+      // Test int component
+      final TypeExpr typeExpr4 = typeExprs[4];
+      final MethodHandle typeExpr4Accessor = accessors[4];
+      testIntRoundTrip(typeExpr4, typeExpr4Accessor);
+
+      // Test long component
+      final TypeExpr typeExpr5 = typeExprs[5];
+      final MethodHandle typeExpr5Accessor = accessors[5];
+      testLongRoundTrip(typeExpr5, typeExpr5Accessor);
+
+      // Test float component
+      final TypeExpr typeExpr6 = typeExprs[6];
+      final MethodHandle typeExpr6Accessor = accessors[6];
+      testFloatRoundTrip(typeExpr6, typeExpr6Accessor);
+
+      // Test double component
+      final TypeExpr typeExpr7 = typeExprs[7];
+      final MethodHandle typeExpr7Accessor = accessors[7];
+      testDoubleRoundTrip(typeExpr7, typeExpr7Accessor);
     }
   }
 }
